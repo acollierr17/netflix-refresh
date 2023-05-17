ac8d9c4b0bdf42b8d9b7ad504cf84a5736ac701c
Anthony Collier <hello@acollier.dev>
2023-05-17 16:12:14 -0400

    chore: configure shadcn/ui and next-theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { type AppType } from "next/app";
+import type { AppType } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider forcedTheme="dark" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
