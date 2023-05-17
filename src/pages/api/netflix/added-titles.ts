85d97fa1e93d4c6287454fc4a394d923f51276dd
Anthony Collier <hello@acollier.dev>
2023-05-17 11:07:02 -0400

    feat(api): add api routes for various netflix utilities
    - All routes are cached at the function level
    - All routes have API key middleware
    - All routes have the ability to pass in a date string for querying with appropriate validation

---
+++ src/pages/api/netflix/added-titles.ts
@@ -0,0 +1,49 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { fetchNewTitles } from "../../../utils/netflix";
+import { convertUTCStringToDate, getFormattedDate } from "../../../utils/date";
+import authenticateRequest from "../../../server/authenticateRequest";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const date = new Date();
+    const queryDate = req.query.date
+      ? convertUTCStringToDate(req.query.date as string)
+      : new Date();
+    if (!queryDate)
+      return res.status(400).json({
+        metadata: {
+          nonce: date.getTime(),
+        },
+        message: `The 'date' query parameter is not in the proper format! (ex. ${getFormattedDate(
+          date
+        )})`,
+      });
+
+    const formattedQueryDate = getFormattedDate(queryDate);
+    const titles = await fetchNewTitles(queryDate);
+    if (titles.length < 1) {
+      res.statusCode = 204;
+      res.statusMessage =
+        "There are no new added titles for the provided date.";
+      return res.end();
+    }
+
+    return res.json({
+      metadata: {
+        nonce: date.getTime(),
+        query_date: formattedQueryDate,
+        size: titles.length,
+      },
+      data: titles,
+    });
+  } catch (e: any) {
+    return res.status(500).json({
+      code: res.statusCode,
+      name: e.name,
+      message: e.message,
+    });
+  }
+}
+
+export default authenticateRequest(handler);
---
+++ src/pages/api/netflix/get-daily-titles.ts
@@ -0,0 +1,51 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { fetchDailyTitles } from "../../../utils/netflix";
+import { convertUTCStringToDate, getFormattedDate } from "../../../utils/date";
+import authenticateRequest from "../../../server/authenticateRequest";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const date = new Date();
+    const queryDate = req.query.date
+      ? convertUTCStringToDate(req.query.date as string)
+      : new Date();
+    if (!queryDate)
+      return res.status(400).json({
+        metadata: {
+          nonce: date.getTime(),
+        },
+        message: `The 'date' query parameter is not in the proper format! (ex. ${getFormattedDate(
+          date
+        )})`,
+      });
+
+    const formattedQueryDate = getFormattedDate(queryDate);
+    const titles = await fetchDailyTitles(queryDate);
+    if (!titles.size) {
+      res.statusCode = 204;
+      res.statusMessage = "No newly added or deleted content.";
+      return res.end();
+    }
+
+    return res.json({
+      metadata: {
+        nonce: date.getTime(),
+        query_date: formattedQueryDate,
+        size: titles.size,
+      },
+      data: {
+        added: titles.added,
+        deleted: titles.deleted,
+      },
+    });
+  } catch (e: any) {
+    return res.status(500).json({
+      code: res.statusCode,
+      name: e.name,
+      message: e.message,
+    });
+  }
+}
+
+export default authenticateRequest(handler);
