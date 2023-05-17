322352f35e46bd260580b59a6fd49aab79bb0a2c
Anthony Collier <hello@acollier.dev>
2023-05-17 10:48:19 -0400

    feat: redis

---
+++ src/server/redis.ts
@@ -0,0 +1,9 @@
+import { Redis } from "@upstash/redis";
+import { env } from "../env/server.mjs";
+
+const redis = new Redis({
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default redis;
