945404641ef0b8c4e2ea02f78fff9cb11a4a0930
Anthony Collier <hello@acollier.dev>
2023-06-15 18:06:29 -0400

    fix(api): fix typings of trpc context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,10 +2,13 @@ import {
   type FetchCreateContextFnOptions,
   fetchRequestHandler,
 } from "@trpc/server/adapters/fetch";
-// import { appRouter } from "../trpc-router";
 import { appRouter } from "@/server/api/root";
 import { env } from "@/env.mjs";
 import { prisma } from "@/server/db";
+
+type CreateContextOptions = {
+  prisma: typeof prisma;
+};
 
 const handler = (request: Request) => {
   console.log(`incoming request ${request.url}`);
@@ -13,11 +16,9 @@ const handler = (request: Request) => {
     endpoint: "/api/trpc",
     req: request,
     router: appRouter,
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore we're just testing the router rq
     createContext: function (
       opts: FetchCreateContextFnOptions
-    ): object | Promise<object> {
+    ): CreateContextOptions {
       return {
         prisma,
       };
