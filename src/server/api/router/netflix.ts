20d2d36e46e0b50041c7070669bb934c693b289f
Anthony Collier <hello@acollier.dev>
2023-07-01 02:27:01 -0400

    feat(api): netflix api routes
    - add route for getting title information
    - add route for getting daily titles

---
+++ src/server/api/router/netflix.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { fetchDailyTitles } from "@/lib/netflix";
+
+export const netflixRouter = createTRPCRouter({
+  getTitle: publicProcedure
+    .input(z.object({ netflixId: z.number() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.title.findUnique({
+        where: {
+          netflixId: input.netflixId,
+        },
+      });
+    }),
+  getDailyTitles: publicProcedure
+    .input(z.object({ date: z.date() }))
+    .query(({ input }) => {
+      return fetchDailyTitles({ date: input.date });
+    }),
+});
